1bd09e79e9dffec0eaf2fd32e526ff43c7d74fe4
HernanIbarra <ibarra.veizaga.hernan@gmail.com>
2020-02-04 09:49:46 -0400

    act
    act

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,19 +7,13 @@ import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-// import { AreaComponent } from './widgets/area/area.component';
-// import { HighchartsChartModule } from 'highcharts-angular';
-// import { CardComponent } from './widgets/card/card.component';
-// import { PieComponent } from './widgets/pie/pie.component';
+
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
     SidebarComponent
-    // AreaComponent,
-    // CardComponent,
-    // PieComponent
   ],
   imports: [
     CommonModule,
@@ -31,15 +25,13 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
     MatMenuModule,
     MatListModule,
     RouterModule
-    // HighchartsChartModule
+
   ],
   exports: [
     HeaderComponent,
     FooterComponent,
     SidebarComponent
-    // AreaComponent,
-    // CardComponent,
-    // PieComponent
+
   ]
 })
 export class SharedModule { }
